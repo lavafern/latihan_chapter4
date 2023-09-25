4ec2b3b6cd11078a15a17084d6b3bd63d57e0f47
rian <rian.rafli@gmail.com>
2023-09-25 22:56:25 +0700

    add destroy and show feature

--- chapter-4/1_file-system/helpers/crud.js
+++ chapter-4/1_file-system/helpers/crud.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const posts = require('../database/posts');
 const PostModel = require('../models/post');
+const { profile, error } = require('console');
 
 function create(title, body) {
     let newPost = new PostModel(posts.id++, title, body);
@@ -10,8 +11,35 @@ function create(title, body) {
 }
 
 function index() { }
-function show(id) { }
+
+
+function show(id) {
+    try {
+        const data = posts.data
+        const foundUser =  data.find(profile => profile.id === id)
+        if (foundUser === undefined) throw new Error(`id ${id} is not exist`)
+        console.log(foundUser)
+    } catch(err) {
+        console.log(err);
+    }
+ } 
+
+
 function update(id, title, body) { }
-function destroy(id) { }
+
+
+function destroy(id) {
+    try {
+        const check = posts.data.find(profile => profile.id===id)
+        if (check ===  undefined) throw new Error(`id ${id} is not exist`)
+
+        posts.data = posts.data.filter(profile => profile.id !== id)
+        fs.writeFileSync('./database/posts.json',JSON.stringify(posts,null,4))
+        console.log(`succes destroy user id ${id} `);
+    } catch(err) {
+        console.log(err);
+    }
+
+ }
 
 module.exports = { create, index, show, update, destroy };
