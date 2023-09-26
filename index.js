35229167b814230230fd6cc22d08d474719fd03d
Muhammad Afif <mafif357@gmail.com>
2023-09-27 00:59:11 +0700

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+const { create, index, show, update, destroy } = require('./helpers/crud');
+
+// test create post
+// create('test title', 'test data');
+
+// test show all post
+index()
+// test show detail post by id
+// show(3)
+// test update post by id
+// test delete post by id
